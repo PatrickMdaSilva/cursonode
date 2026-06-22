bf05097018085148412db08bc3f8fa90ba2b28d3
PatrickMdaSilva <patrick15k@hotmail.com>
2026-06-21 21:01:56 -0300

    package.json

--- 002_servidor/server.js
+++ 002_servidor/server.js
@@ -1,11 +1,17 @@
+// Criando servidor web
+
+// Protocolo
 const http = require("http")
+// IP
 const host = "127.0.0.1"
+// Porta utilizada
 const port = 3000
 
+// Inicialização do servidor envio de requisição e retorno de resposta
 const server = http.createServer((req, res)=>{
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Ola mundo \n Meu Primeiro Script');
+    res.end(`Ola mundo \n Meu Primeiro Script\n Status: ${res.statusCode}`);
 })
 
 server.listen(port, host,()=>{
